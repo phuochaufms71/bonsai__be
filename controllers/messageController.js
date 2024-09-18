6dbab61e60bd4cfc3bf93a9ed2e4db940b714176
phuochaufms71 <phuochaubmw@gmail.com>
2024-09-18 10:05:41 +0700

    fix

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -20,7 +20,7 @@ export const createMessage = async (req, res) => {
     }
     const today = new Date();
     var date = today.getDate();
-    var month = today.getMonth();
+    var month = today.getMonth() + 1;
     var year = today.getFullYear();
     var hours = today.getHours();
     var minutes = today.getMinutes();
